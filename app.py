68cc85136502db9fe6562e7fce27c025c18d5ebb
ssvergara <sandravergara115@gmail.com>
2019-07-03 13:23:18 +0800

    Add index.html and dashboard

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask, render_template, request
+from flask import Flask, render_template, request
 from pusher import Pusher
 
 app = Flask(__name__)
 
+
+# Configre pusher object
 pusher = Pusher(
 app_id = "814733",
 key = "71198b6c72378c71016c",
 secret = "2c0ab920339934fc784a",
 cluster = "ap1",
-ssl=True
-)
+ssl=True)
 
 @app.route('/')
 def index():
@@ -20,7 +21,34 @@ def index():
 def dashboard():
     return render_template('dashboard.html')
 
-@app.route('orders', method=['POST'])
+@app.route('/orders', methods=['POST'])
 def order():
     data = request.form
-    pusher.trigger(u'order', u.place)
+    pusher.trigger(u'order', u'place', {
+        u'units': data['units']
+    })
+    return "units logged"
+
+@app.route('/message', methods=['POST'])
+def message():
+    data = request.form
+    pusher.trigger(u'message', u'send', {
+        u'name': data['name'],
+        u'message': data['message']
+    })
+    return "message sent"
+
+@app.route('/customer', methods=['POST'])
+def customer():
+    data = request.form
+    pusher.trigger(u'customer', u'add', {
+        u'name': data['name'],
+        u'position': data['position'],
+        u'office': data['office'],
+        u'age': data['age'],
+        u'salary': data['salary'],
+    })
+    return "customer added"
+
+if __name__ == '__main__':
+    app.run(debug=True)
